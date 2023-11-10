9e42378a75d6a1452b6f3ab1a12bbe93a9cf10bb
Knight <kangrack333@gmail.com>
2023-11-10 22:11:56 +0900

    refactor : 날짜 유효성검사 클래스 매직넘버 수정

--- src/main/java/christmas/validator/DayValidator.java
+++ src/main/java/christmas/validator/DayValidator.java
@@ -2,6 +2,7 @@ package christmas.validator;
 
 public class DayValidator implements Validator {
 
+    private final static String INVALID_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private final static String NUMBER_REGULAR_EXPRESSION = "^[0-9]+$";
     private final static int DAY_NUM_MIN_RANGE = 1;
     private final static int DAY_NUM_MAX_RANGE = 31;
@@ -14,14 +15,14 @@ public class DayValidator implements Validator {
 
     public void throwIsNotNumber(String input) {
         if (!input.matches(NUMBER_REGULAR_EXPRESSION)) {
-            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_DATE);
         }
     }
 
     public void throwIsNotRange(String input) {
         int day = Integer.parseInt(input);
         if (day < DAY_NUM_MIN_RANGE || DAY_NUM_MAX_RANGE < day) {
-            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException(INVALID_DATE);
         }
     }
 
